20aab4b68daa2a4f5e46b91218a13b97881c129b
Windryuu <brayan.jean@laposte.net>
2021-09-07 16:02:15 +0200

    a

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-define("ROOT",realpath(__DIR__.DIRECTORY_SEPARATOR.'..'));
+
 define("HTTP", ($_SERVER["SERVER_NAME"] == "localhost")
    ? "http://localhost:8000/"
    : "http://your_site_name.com/"
 );
-define("TEMPLATES",realpath(ROOT.DIRECTORY_SEPARATOR ."src" . DIRECTORY_SEPARATOR . "templates"));
-
 
 require 'vendor/autoload.php';
 
@@ -13,7 +11,6 @@ use App\Controller\UserController;
 use App\Core\Request;
 use App\Core\Router;
 use App\Core\Session;
-use App\Entity\User;
 use App\Exception\RouterException;
 use App\Repository\UserRepository;
 
@@ -21,7 +18,7 @@ session_start();
 $session = new Session(
     isset($_SESSION["user"]) ? unserialize($_SESSION["user"]) : null
 );
-//dump($_SESSION);
+
 ?>
 
 <a href="<?=HTTP?>"><button>Accueil</button></a>
@@ -34,10 +31,7 @@ $session = new Session(
 <?php endif; ?>
 
 
-
-
 <?php
-
 
 //initialise la request
 $request = new Request();
@@ -50,7 +44,7 @@ $router->add("lol",function(){echo 'Bro wtf';},$request->getMethod());
 $router->add("signup",function(){(new UserRepository()); (new UserController())->userSignup();},$request->getMethod());
 $router->add("signin",function(){(new UserRepository());(new UserController())->userSignin();},$request->getMethod());
 $router->add("signout",function(){(new UserRepository()); (new UserController())->userSignout();},$request->getMethod());
-$router->add("user/:id/show",function(){(new UserRepository()); (new UserController())->userShow(unserialize($_SESSION["user"])->getId_user());},$request->getMethod());
+$router->add("user/show",function(){(new UserRepository()); (new UserController())->userShow(unserialize($_SESSION["user"])->getId_user());},$request->getMethod());
 $router->add("user/update",function(){(new UserRepository()); (new UserController())->userUpdate(unserialize($_SESSION["user"])->getId_user());},$request->getMethod());
 //on lance notre application
 try {
