c2cb02f1141f8d2e5b412c012f6ca7976a4a7688
B0UBA <moussierquentin@gmail.com>
2021-09-08 11:41:37 +0200

    bug fix scss

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
-define("HTTP", ($_SERVER["SERVER_NAME"] == "localhost")
-   ? "http://localhost:8000/"
-   : "http://your_site_name.com/"
+define(
+    "HTTP",
+    ($_SERVER["SERVER_NAME"] == "localhost")
+        ? "http://localhost:8000/"
+        : "http://your_site_name.com/"
 );
 
 require 'vendor/autoload.php';
@@ -23,44 +25,69 @@ $session = new Session(
 
 ?>
 
-<a href="<?=HTTP?>"><button>Accueil</button></a>
+<a href="<?= HTTP ?>"><button>Accueil</button></a>
 <?php if (!isset($_SESSION["user"])) : ?>
-<a href="<?=HTTP?>signup"><button>S'enregistrer</button></a>
-<a href="<?=HTTP?>signin"><button>Se connecter</button></a>
+    <a href="<?= HTTP ?>signup"><button>S'enregistrer</button></a>
+    <a href="<?= HTTP ?>signin"><button>Se connecter</button></a>
 <?php else : ?>
-<a href="<?=HTTP?>user<?= DIRECTORY_SEPARATOR?>show"><button>Afficher mon profil</button></a>
-<a href="<?=HTTP?>signout"><button>Se déconnecter</button></a>
+    <a href="<?= HTTP ?>user<?= DIRECTORY_SEPARATOR ?>show"><button>Afficher mon profil</button></a>
+    <a href="<?= HTTP ?>signout"><button>Se déconnecter</button></a>
 <?php endif; ?>
 
 
 <?php
+  //initialise la request
+    $request = new Request();
 
-//initialise la request
-$request = new Request();
-//initialisation de notre router
-$router = new Router($request);
-$artController = new ArticleController();
-$categController = new CategorieController();
-//on ajoute les routes dispo dans l'appli
+if ($request->getFilenameExtension() === "png" || $request->getFilenameExtension() === "jpg") {
+    header("Content-type: image/png");
+    readfile($request->getScriptFileName());
+} else if ($request->getFilenameExtension() === "css") {
+    header("Content-type: text/css");
+    readfile($request->getScriptFileName());
+} else {
 
 
-$router->add("articles",[$artController, 'index'],$request->getMethod());
-$router->add("ficheproduit/:id", [$artController, "show"], $request->getMethod());
-$router->add("categorie/:id",[$categController, 'index'],$request->getMethod());
-$router->add("search/:word",[$artController, 'search'],$request->getMethod());
+    //initialisation de notre router
+    $router = new Router($request);
+    $artController = new ArticleController();
+    $categController = new CategorieController();
+    //on ajoute les routes dispo dans l'appli
 
-//on ajoute les routes dispo dans l'appli
 
-$router->add("signup",function(){(new UserRepository()); (new UserController())->userSignup();},$request->getMethod());
-$router->add("signin",function(){(new UserRepository());(new UserController())->userSignin();},$request->getMethod());
-$router->add("signout",function(){(new UserRepository()); (new UserController())->userSignout();},$request->getMethod());
-$router->add("user/show",function(){(new UserRepository()); (new UserController())->userShow(unserialize($_SESSION["user"])->getId_user());},$request->getMethod());
-$router->add("user/update",function(){(new UserRepository()); (new UserController())->userUpdate(unserialize($_SESSION["user"])->getId_user());},$request->getMethod());
-//on lance notre application
-try {
-    $router->run($request);
-} catch (RouterException $e) {
-    echo $e->getMessage();
+    $router->add("articles", [$artController, 'index'], $request->getMethod());
+    $router->add("ficheproduit/:id", [$artController, "show"], $request->getMethod());
+    $router->add("categorie/:id", [$categController, 'index'], $request->getMethod());
+    $router->add("search/:word", [$artController, 'search'], $request->getMethod());
+
+    //on ajoute les routes dispo dans l'appli
+
+    $router->add("signup", function () {
+        (new UserRepository());
+        (new UserController())->userSignup();
+    }, $request->getMethod());
+    $router->add("signin", function () {
+        (new UserRepository());
+        (new UserController())->userSignin();
+    }, $request->getMethod());
+    $router->add("signout", function () {
+        (new UserRepository());
+        (new UserController())->userSignout();
+    }, $request->getMethod());
+    $router->add("user/show", function () {
+        (new UserRepository());
+        (new UserController())->userShow(unserialize($_SESSION["user"])->getId_user());
+    }, $request->getMethod());
+    $router->add("user/update", function () {
+        (new UserRepository());
+        (new UserController())->userUpdate(unserialize($_SESSION["user"])->getId_user());
+    }, $request->getMethod());
+    //on lance notre application
+    try {
+        $router->run($request);
+    } catch (RouterException $e) {
+        echo $e->getMessage();
+    }
 }
 
 
